25e9d61ad8308b2a616f3e0dc3379b46621e800f
Sebastián Carabali <sebastiancc@unicauca.edu.co>
2020-11-05 12:55:14 -0500

    Mensaje

--- zebir/settings.py
+++ zebir/settings.py
@@ -140,4 +140,6 @@ MEDIA_ROOT = (
 MEDIA_URL = '/media/'
 
 LOGIN_URL = '/login'
-i
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
